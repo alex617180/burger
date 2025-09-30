24b5b488ed763a84de1037932a95d52618d78b57
Alexey Sannikov <alex617180@mail.ru>
2025-09-30 16:26:40 +0500

    change: refactoring app and app-header components.

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,40 +1,35 @@
-import React, { useState } from 'react';
 import { BurgerIcon, ListIcon, Logo, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
-import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 
 
-export default function AppHeader(){
-    const [activeSection, setActiveSection] = useState('burger-constructor');
+export default function AppHeader(props){
     return (
-    <div className="font-sans">
-        {/* Фиксированная шапка */}
         <header className="sticky top-0 bg-zinc-900 text-white z-50">
             <nav className="max-w-5xl mx-auto flex items-center justify-between px-4 h-16">
                 <ul className="flex gap-6 list-none">
                     <li className="flex items-center gap-2">
-                        <BurgerIcon type={activeSection === "burger-constructor" ? "primary" : "secondary"} />
-                        <button onClick={() => setActiveSection("burger-constructor")}
+                        <BurgerIcon type={props.activeSection === "burger-constructor" ? "primary" : "secondary"} />
+                        <button onClick={() => props.setActiveSection("burger-constructor")}
                                 className={`hover:text-gray-200 text text_type_main-default 
-                                        ${activeSection !== "burger-constructor" ? "text_color_inactive" : ""}`} 
+                                        ${props.activeSection !== "burger-constructor" ? "text_color_inactive" : ""}`} 
                         >
                             Конструктор
                         </button>
                     </li>
                     <li className="flex items-center gap-2">
-                        <ListIcon type={activeSection === "order-feed" ? "primary" : "secondary"} />
-                        <button onClick={() => setActiveSection("order-feed")}
+                        <ListIcon type={props.activeSection === "order-feed" ? "primary" : "secondary"} />
+                        <button onClick={() => props.setActiveSection("order-feed")}
                                 className={`hover:text-gray-200 text text_type_main-default 
-                                        ${activeSection !== "order-feed" ? "text_color_inactive" : ""}`}
+                                        ${props.activeSection !== "order-feed" ? "text_color_inactive" : ""}`}
                         >
                             Лента заказов
                         </button>
                         </li>
                     <li className="ml-10 mr-25"><Logo /></li>
                     <li className="flex items-center gap-2">
-                        <ProfileIcon type={activeSection === "personal-account" ? "primary" : "secondary"} />
-                        <button onClick={() => setActiveSection("personal-account")}
+                        <ProfileIcon type={props.activeSection === "personal-account" ? "primary" : "secondary"} />
+                        <button onClick={() => props.setActiveSection("personal-account")}
                                 className={`hover:text-gray-200 text text_type_main-default 
-                                        ${activeSection !== "personal-account" ? "text_color_inactive" : ""}`}
+                                        ${props.activeSection !== "personal-account" ? "text_color_inactive" : ""}`}
                         >
                             Личный кабинет
                         </button>
@@ -42,20 +37,5 @@ export default function AppHeader(){
                 </ul>
             </nav>
         </header>
-    
-        <main className="max-w-5xl mx-auto px-4">
-            {activeSection === "burger-constructor" && (<section className="scroll-mt-16 py-8">
-                <BurgerIngredients />
-            </section>)}
-
-            {activeSection === "order-feed" && (<section className="scroll-mt-16 py-8">
-                <h2 className="text-2xl font-bold mb-4">2</h2>
-            </section>)}
-
-            {activeSection === "personal-account" && (<section className="scroll-mt-16 py-8">
-                <h2 className="text-2xl font-bold mb-4">3</h2>
-            </section>)}
-        </main>
-    </div>
   );
 }
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,35 @@
 import React, { useState } from 'react';
 import ErrorBoundary from '../error-boundary/error-boundary.jsx'
+import BurgerIngredients from '../burger-ingredients/burger-ingredients.jsx';
+import AppHeader from '../app-header/app-header.jsx';
 
 function App(){
     const [state, setState] = useState();
+    const [activeSection, setActiveSection] = useState('burger-constructor');
+    
     const apiUrlIngredients = 'https://norma.nomoreparties.space/api/ingredients';
 
     return (
         <div className="App">
         <ErrorBoundary>
-            <AppHeader />
+                <div className="font-sans">
+                    {/* Фиксированная шапка */}
+                    <AppHeader setActiveSection={setActiveSection} activeSection={activeSection}/>
+                
+                    <main className="max-w-5xl mx-auto px-4">
+                        {activeSection === "burger-constructor" && (<section className="scroll-mt-16 py-8">
+                            <BurgerIngredients />
+                        </section>)}
+            
+                        {activeSection === "order-feed" && (<section className="scroll-mt-16 py-8">
+                            <h2 className="text-2xl font-bold mb-4">2</h2>
+                        </section>)}
+            
+                        {activeSection === "personal-account" && (<section className="scroll-mt-16 py-8">
+                            <h2 className="text-2xl font-bold mb-4">3</h2>
+                        </section>)}
+                    </main>
+                </div>            
         </ErrorBoundary>
         </div>
     )
