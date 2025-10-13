86fe1d78912253ecdaa9d3028b69264b8d871d15
Alexey Sannikov <alex617180@mail.ru>
2025-10-13 11:44:41 +0500

    change: refactoring app

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -6,37 +6,26 @@ import BurgerConstructor from '../burger-constructor/burger-constructor.jsx';
 
 function App(){
     const apiUrlIngredients = 'https://norma.nomoreparties.space/api/ingredients';
-    const [state, setState] = useState({
-        activeSection: 'burger-constructor',
-        isLoading: false,
-        hasError: false,
-        ingredientsData: []
-    });
-    
-    function setActiveSection(section){
-        setState({...state, activeSection: section})
-    }
+    const [activeSection, setActiveSection] = useState('burger-constructor');
+    const [ingredientsData, setIngredientsData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [hasError, setHasError] = useState(false);
     
     useEffect(()=>{
         const getIngredients= async() => {
-            setState(prev => ({...prev, isLoading:true, hasError:false}))
+            setIsLoading(true);
+            setHasError(false);
             try {
                 const result = await fetch(apiUrlIngredients);
                 if (!result.ok) throw new Error(`Ошибка http: ${result.status}`);
                     
                 const data = await result.json();
-                setState(prev => ({
-                    ...prev,
-                    ingredientsData: data.data ?? [],
-                    isLoading: false,
-                }))
+                setIngredientsData(data.data ?? []);
             } catch (error) {
                 console.error(error);
-                setState(prev => ({
-                    ...prev,
-                    isLoading: false,
-                    hasError: true,
-                }));
+                setHasError(true);
+            } finally {
+                setIsLoading(false);
             }
         }
         getIngredients();        
@@ -47,31 +36,31 @@ function App(){
         <ErrorBoundary>
             <div className="font-sans">
                 {/* Фиксированная шапка */}
-                <AppHeader setActiveSection={setActiveSection} activeSection={state.activeSection}/>
+                <AppHeader setActiveSection={setActiveSection} activeSection={activeSection}/>
             
                 <main className="max-w-5xl mx-auto px-4">
-                    {state.activeSection === "burger-constructor" && (<section className="scroll-mt-16 py-8">
-                        {state.isLoading && 'Загрузка...'}
-                        {state.hasError && 'Произошла ошибка, обновите страницу. Если после 3 попыток проблема не исчезнет, то закройте вкладку и не возвращайтесь.'}
-                        {!state.isLoading && !state.hasError && state.ingredientsData.length &&                                
+                    {activeSection === "burger-constructor" && (<section className="scroll-mt-16 py-8">
+                        {isLoading && 'Загрузка...'}
+                        {hasError && 'Произошла ошибка, обновите страницу. Если после 3 попыток проблема не исчезнет, то закройте вкладку и не возвращайтесь.'}
+                        {!isLoading && !hasError && ingredientsData.length > 0 &&
                             (<div className="flex h-screen">
                                 {/* Левая половина */}
                                 <div className="w-1/2 flex flex-col">
-                                    <BurgerIngredients ingredients={state.ingredientsData} />
+                                    <BurgerIngredients ingredients={ingredientsData} />
                                 </div>
                                 {/* Правая половина */}
                                 <div className="w-1/2 p-5">
-                                    <BurgerConstructor ingredients={state.ingredientsData} />
+                                    <BurgerConstructor currentIngredients={ingredientsData} orderData={{}} />
                                 </div>
                             </div>)
                         }
                     </section>)}
         
-                    {state.activeSection === "order-feed" && (<section className="scroll-mt-16 py-8">
+                    {activeSection === "order-feed" && (<section className="scroll-mt-16 py-8">
                         <h2 className="text-2xl font-bold mb-4">2</h2>
                     </section>)}
         
-                    {state.activeSection === "personal-account" && (<section className="scroll-mt-16 py-8">
+                    {activeSection === "personal-account" && (<section className="scroll-mt-16 py-8">
                         <h2 className="text-2xl font-bold mb-4">3</h2>
                     </section>)}
                 </main>
